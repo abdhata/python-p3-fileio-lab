0ac40cf5b5dc82ba28e26df9e0bd5fe341cb5e3b
abdhata <abdhata@outlook.com>
2023-09-04 22:52:22 +0300

    Completed

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,12 @@
-def write_file(file_name, file_content):
-    pass
+def write_file(file_name="lib/employees", file_content="employee1: Abdi Hassan - Software Engineer"):
+    with open(f'{file_name}.txt', mode='w', encoding='utf-8') as employee :
+        employee.write(f'{file_content}')
+        pass
 
-def append_file(file_name, append_content):
-    pass
+def append_file(file_name="lib/employees", append_content="employee2: Amalito - CTO"):
+    with open(f'{file_name}.txt', mode='a', encoding='utf-8') as employee:
+        employee.write(f'{append_content}') 
 
-def read_file(file_name):
-    pass
+def read_file(file_name="lib/employees"):
+    with open(f'{file_name}.txt', encoding='utf-8') as employee:
+         return employee.read()
